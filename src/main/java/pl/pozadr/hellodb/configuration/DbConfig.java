76b09e1880ca97873b2bcffad2afad05b578b01e
Pozadr <adpozor@gmail.com>
2020-12-09 13:27:39 +0100

    DBConfig -> create table

--- src/main/java/pl/pozadr/hellodb/configuration/DbConfig.java
+++ src/main/java/pl/pozadr/hellodb/configuration/DbConfig.java
@@ -1,14 +1,20 @@
 package pl.pozadr.hellodb.configuration;
 
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DbConfig {
 
-    private DataSource getDataSource() {
+    @Bean
+    public DataSource getDataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.url("jdbc:mysql://remotemysql.com:3306/N8Ilk2pIr1");
         dataSourceBuilder.username("N8Ilk2pIr1");
@@ -16,4 +22,18 @@ public class DbConfig {
         dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
         return dataSourceBuilder.build();
     }
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate() {
+        return new JdbcTemplate(getDataSource());
+    }
+
+
+    // CREATE TABLE
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        String sql = "CREATE TABLE videos(video_id int, title varchar(255), url varchar(255), PRIMARY KEY (video_id))";
+        getJdbcTemplate().update(sql);
+    }
+
 }
